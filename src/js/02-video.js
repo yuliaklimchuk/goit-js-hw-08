355708fabeedc91af4b7151b36460415401dbf4c
Yulia Klimchuk <yulia.melnichenko.995@gmail.com>
2022-01-29 17:55:16 +0200

    add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash/throttle';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+let playerSecond;
+const startVideoSecond = localStorage.getItem("videoplayer-current-time") ?? 0;
+
+player.setCurrentTime(startVideoSecond);
+
+player.on('timeupdate', throttle(function (data) {
+    playerSecond = data.seconds;
+    localStorage.setItem('videoplayer-current-time', playerSecond);
+}, 1000));
+
 
